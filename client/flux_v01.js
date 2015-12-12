88d2f0d80aaec2201ca691add63082b1a4eb9471
angel imaz <angel.imaz@hush.com>
2015-12-12 14:24:31 +0000

    on and off switch

--- client/flux_v01.js
+++ client/flux_v01.js
@@ -1,38 +1,43 @@
+// counter starts at 0
+Session.setDefault('toggle1', false);
+var devId = '260039000347343233323032';
 
-  // counter starts at 0
-  //Session.setDefault('counter', 0);
 Template.hello.rendered = function() {
-    // init.
-    spark.login({accessToken: 'fad8cca1f42bd758b7c5684e665123d41c38b632'});
+  // init.
+  spark.login({
+    accessToken: 'fad8cca1f42bd758b7c5684e665123d41c38b632'
+  });
 
+}
+
+Template.hello.helpers({
+  counter: function() {
+    return Session.get('counter');
+  },
+  
+  checked: function() {
+    return Session.get('toggle1');
   }
   
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      var devId = '260039000347343233323032';
-    spark.callFunction(devId, 'led', 'on', callback);
-      
-      
-     // Session.set('counter', Session.get('counter') - 2);
-    }
-  });
   
-  var callback = function(err, data) {
-    if (err) {
-      console.log('An error occurred while getting core attrs:', err);
-    } else {
-      console.log('Core attr retrieved successfully:', data);
+});
+
+Template.hello.events({
+
+  "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      console.log("toggle checked");
+      spark.callFunction(devId, 'led', Session.get('toggle1')?'off':'on', callback);
+      Session.set('toggle1', ! Session.get('toggle1'));
     }
-  };
-  
+});
 
-
-
-
+var callback = function(err, data) {
+  if (err) {
+    console.log('An error occurred while getting core attrs:', err);
+  }
+  else {
+    console.log('Core attr retrieved successfully:', data);
+    
+  }
+};
